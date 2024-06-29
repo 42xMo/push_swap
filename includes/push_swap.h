40527ba1bf7320d104ed6d65eef73f1f256449bc
Mohamed Abdessmad <mabdessm@c1r6p2.42nice.fr>
2024-06-29 12:45:48 +0200

    added check_errors.c

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 17:50:54 by mabdessm          #+#    #+#             */
-/*   Updated: 2024/06/29 11:32:22 by mabdessm         ###   ########.fr       */
+/*   Updated: 2024/06/29 12:45:04 by mabdessm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include "../printf/ft_printf.h"
+
+int	no_error(char **argv, int argc);
+int	no_duplicates(char **argv, int argc);
 
 #endif
---
+++ pushswap/check_errors.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_errors.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabdessm <mabdessm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 12:43:23 by mabdessm          #+#    #+#             */
+/*   Updated: 2024/06/29 12:44:15 by mabdessm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	no_error(char **argv, int argc)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		if (ft_atoi(argv[i]) > 2147483647 || ft_atoi(argv[i]) < -2147483648)
+			return (0);
+		if (ft_atoi(argv[i]) < 0)
+			++j;
+		if (!argv[i][j])
+			return (0);
+		while (argv[i][j])
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				return (0);
+			++j;
+		}
+		++i;
+	}
+	return (1);
+}
+
+int	no_duplicates(char **argv, int argc)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				return (0);
+			++j;
+		}
+		++i;
+	}
+	return (1);
+}
